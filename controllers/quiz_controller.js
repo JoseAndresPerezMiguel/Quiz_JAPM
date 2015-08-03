6ac224c4088625e3ad15070e6c992f8b93187595
Jose Andres Perez <perezmiguel.ja@gmail.com>
2015-08-03 19:11:39 +0200

    Borrar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -66,7 +66,7 @@ exports.create = function(req, res) {
         .then( function(){ res.redirect('/quizes')}) 
       }      // res.redirect: Redirección HTTP a lista de preguntas
     }
-  );
+  ).catch(function(error){next(error)});
 };
 
 // GET /quizes/:id/edit
@@ -93,5 +93,12 @@ exports.update = function(req, res) {
         .then( function(){ res.redirect('/quizes');});
       }     // Redirección HTTP a lista de preguntas (URL relativo)
     }
-  );
+  ).catch(function(error){next(error)});
+};
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+  req.quiz.destroy().then( function() {
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
 };
